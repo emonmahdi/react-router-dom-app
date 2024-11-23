51f84dba20a85c6ee927acadc4e9e4b784699685
emonmahdi <developeremon1997@gmail.com>
2024-11-23 16:00:39 +0600

    product fetch and set Condition error show

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,46 @@
- 
+import { useEffect, useState } from "react";
 
 export const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://dummyjson.com/products")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Data could not be Fetched!");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        console.log(data.products);
+        setProducts(data.products);
+        setIsLoading(false);
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div>
-        <h1>Welcome to Products Page</h1>
+      <h1>Welcome to Products Page</h1>
+      {isLoading && <p>Products Are Loading.........</p>}
+      {error && <p> Some Error! </p>}
+      <section className="product-container">
+        <div>
+          {products.map((product) => {
+            const { id, title } = product;
+            return (
+              <article key={id}>
+                <h3>{title}</h3>
+              </article>
+            );
+          })}
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -5,24 +5,29 @@ const Profile = () => {
   console.log(location.state);
   return (
     <div className="profile-container">
-      <div className="profile-card">
-        <div className="profile-avatar">
-          <img src="ksdfsdf" alt={`'s avatar`} />
-        </div>
-        <div className="profile-info">
-          <h2> Name: {state?.name}</h2>
-          <p>
-            <strong>Email:</strong> {state?.email}
-          </p>
-          <p>
-            <strong>Phone:</strong> {state.phone}
-          </p>
-          <p>
-            <strong>City:</strong> {state.city}
-          </p>
- 
-        </div>
-      </div>
+      {state ? (
+        <>
+          <div className="profile-card">
+            <div className="profile-avatar">
+              <img src="ksdfsdf" alt={`'s avatar`} />
+            </div>
+            <div className="profile-info">
+              <h2> Name: {state?.name}</h2>
+              <p>
+                <strong>Email:</strong> {state?.email}
+              </p>
+              <p>
+                <strong>Phone:</strong> {state.phone}
+              </p>
+              <p>
+                <strong>City:</strong> {state.city}
+              </p>
+            </div>
+          </div>
+        </>
+      ) : (
+        "No User Exits"
+      )}
     </div>
   );
 };
