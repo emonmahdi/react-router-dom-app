433f5bb839c02633b552a83592ba0035a5957b4b
emonmahdi <developeremon1997@gmail.com>
2024-11-25 21:19:30 +0600

    dynamic route using useParams

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -1,41 +1,73 @@
-import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const { state } = useLocation();
   console.log(state);
+
+  const { id } = useParams();
+  console.log(id);
+
+  const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Data could not be Fetched!");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setProduct(data);
+        setIsLoading(false);
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, [id]);
+
   return (
     <div>
-      <article className="single-product">
-        <img
-          src={
-            state.images && state.images.length > 0
-              ? state.images[0]
-              : "https://via.placeholder.com/300"
-          }
-          alt={state.title || "product-image"}
-        />
-        <div className="single-product-content">
-          <h3>{state.title}</h3>
-          <p>
-            <b>Category:</b> {state.category}
-          </p>
-          <p>
-            <b>Rating:</b> {state.rating}
-          </p>
-          <p>
-            <b>Brand:</b> {state.brand}
-          </p>
-          <p>{state.description}</p>
-          <p>
-            <b>Price: </b>
-            {state.price}
-          </p>
-          <p>
-            <b>Stock:</b> {state.stock}
-          </p>
-          <Link to={`/product`}>Go To Shopping</Link>
-        </div>
-      </article>
+      {isLoading && <p>Products Are Loading.........</p>}
+      {error && <p>Some Error!</p>}
+      {!isLoading && !error && (
+        <article className="single-product">
+          <img
+            src={
+              product.images && product.images.length > 0
+                ? product.images[0]
+                : "https://via.placeholder.com/300"
+            }
+            alt={product.title || "product-image"}
+          />
+          <div className="single-product-content">
+            <h3>{product.title}</h3>
+            <p>
+              <b>Category:</b> {product.category}
+            </p>
+            <p>
+              <b>Rating:</b> {product.rating}
+            </p>
+            <p>
+              <b>Brand:</b> {product.brand}
+            </p>
+            <p>{product.description}</p>
+            <p>
+              <b>Price: </b>
+              {product.price}
+            </p>
+            <p>
+              <b>Stock:</b> {product.stock}
+            </p>
+            <Link to={`/product`}>Go To Shopping</Link>
+          </div>
+        </article>
+      )}
     </div>
   );
 };
